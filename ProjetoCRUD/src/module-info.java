c4b4026f3dcf56123233381d59b9119201761d39
LucasGBurch <88804181+LucasGBurch@users.noreply.github.com>
2021-12-23 18:24:46 -0300

    Add files via upload

---
+++ ProjetoCRUD/src/module-info.java
@@ -0,0 +1,40 @@
+module projetoCRUD {
+	
+/*
+ * Projeto Create, Read, Update, Delete (CRUD)
+  
+ * Funcionalidades:
+	 * Criar Pessoa ou Aluno;
+	 * Mostrar todas as pessoas e alunos criadas (listar na tela);
+	 * Atualizar dados de uma pessoa ou aluno;
+	 * Deletar pessoa ou aluno;
+	 * Encerrar programa;
+	 
+ * Informações de uma pessoa/aluno:
+	 * Nome;
+	 * Telefone;
+	 * Data de nascimento;
+	 * Data de cadastro da pessoa;
+	 * Data da última alteração;
+	 * Nota final do curso;
+ 
+ * Caso o usuário informe a nota final do curso, deve ser criado
+ um aluno. Caso não seja informada, deve ser criada uma pessoa.
+ 
+ * Criar menu interativo que o usuário possa usar para executar
+ as opções acima;
+ 
+ * O menu deve continuar sendo exibido até que o usuário use a
+ opção de encerrar a aplicação;
+ 
+ * Usar arrays para armazenar as informações criadas;
+  
+ * Usar herança para que um aluno aproveite todos os dados da
+ pessoa;
+ 
+ * Todas as informações ficam em memória. Não é necessário
+ cadastrar em banco de dados. Ou seja, quando encerrar a aplicação,
+ perde-se todo o histórico.
+ 
+ */
+}
